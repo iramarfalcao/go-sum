1fe042213df31b0102e7889fc7469609b74e1fd6
iramafalcao <iramar.j@hotmail.com>
2022-02-13 21:26:41 -0300

    Function of Sum

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/iramarfalcao/go-sum
+
+go 1.17
---
+++ math_test.go
@@ -0,0 +1,10 @@
+package main
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	total := Sum(15, 15)
+	if total != 30 {
+		t.Errorf("Result of Sum is invalid! Result: %d, Expected: %d", total, 30)
+	}
+}
